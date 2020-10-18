7c61098dcf7477b994c334af43c5ad6d36967697
Yen-Chi Chen <ycchen@Yen-Chis-Mac-mini.local>
2020-10-18 22:41:27 +0200

    simplify test

--- src/import.test.js
+++ src/import.test.js
@@ -5,10 +5,6 @@ test('mocking importing', async () => {
     const caller = await import('./importing');
     jest.mock('./imported');
     const callee = require('./imported').default;
-    console.log(caller);
-    console.log(callee);
-    //callee.mockImplementation(() => '42');
     await caller.default();
     return expect(callee).toHaveBeenCalled();
-    //return expect(caller()).toEqual('42');
 });
